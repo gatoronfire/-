c90b66f68b45e2f65e90aaaa8283615100761bae
Mark-kus <tignanellimarco@gmail.com>
2022-12-19 13:36:21 -0300

    cleaning

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.static('public'))
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index', {haikus: haikus});
+  res.render('index');
 });
 
 app.listen(3000, () => {
