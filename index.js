869eb998431bcac98d15950c2f2f98c99942f290
Mark-kus <tignanellimarco@gmail.com>
2022-12-21 11:55:19 -0300

    boilerplate

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 let express = require('express');
 let app = express();
 let ejs = require('ejs');
+let ejsMate = require('ejs-mate')
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs');
+app.engine('ejs', ejsMate);
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
 });
 
 app.listen(3000, () => {
