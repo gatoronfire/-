5a9201f16f51de3b31746d40ecbaaf1ff2c09342
Mark-kus <tignanellimarco@gmail.com>
2022-12-21 14:29:50 -0300

    seeds added, contact changed

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 let express = require('express');
 let app = express();
 let ejs = require('ejs');
-let ejsMate = require('ejs-mate')
+let ejsMate = require('ejs-mate');
+let { currentUsers } = require('./seeds/usuarios');
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.engine('ejs', ejsMate);
 
@@ -11,8 +12,8 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/contact', (req, res) => {
-  res.render('contact');
+app.get('/contact', async (req, res) => {
+  res.render('contact', { currentUsers });
 });
 
 app.get('/location', (req, res) => {
@@ -20,5 +21,5 @@ app.get('/location', (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log('Listening at port 3000')
+  console.log('Listening at port 3000');
 });
---
+++ seeds/usuarios.js
@@ -0,0 +1,20 @@
+module.exports.currentUsers = {
+    sirDoge: {
+        gei: true,
+        isAdmin: true,
+        description: 'Un tierno perro de 3 cabeza, devora hombres',
+        humble: true
+    },
+    gatorOnFire: {
+        gei: true,
+        isAdmin: false,
+        description: 'Aún con ese chicho, no existe mayor devora hombres que él',
+        humble: false
+    },
+    martha: {
+        gei: true,
+        isAdmin: true,
+        description: 'La mujer del servidor, no es realmente una mujer, o si?',
+        humble: false
+    }
+}
