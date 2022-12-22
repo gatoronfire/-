183dfc873ce0ec5d0fc0b310763bca905ab4602e
gatoronfire <diego.casas11104@gmail.com>
2022-12-22 10:58:02 -0300

    name variable added

--- seeds/usuarios.js
+++ seeds/usuarios.js
@@ -1,23 +1,27 @@
 module.exports.currentUsers = {
     sirDoge: {
+        name:'sirDoge',
         gei: true,
         isAdmin: true,
         description: 'Un tierno perro de 3 cabeza, devora hombres',
         humble: true
     },
     gatorOnFire: {
+        name:'gatorOnFire',
         gei: true,
         isAdmin: false,
         description: 'Aún con ese chicho, no existe mayor devora hombres que él',
         humble: false
     },
     martha: {
+        name:'martha',
         gei: true,
         isAdmin: true,
         description: 'La mujer del servidor, no es realmente una mujer, o si?',
         humble: false
     },
     ElPelucaChoripanero: {
+        name:'ElPelucaChoripanero',
         gei: true,
         isAdmin: false,
         description: 'Un enano caliente por varios motivos, "con peron esto no pasaba"',
