1b5afd3ac420956b46b127f4f8caf0decc81f78e
Mark-kus <tignanellimarco@gmail.com>
2023-01-28 12:18:53 -0300

    seeds

--- seeds/roles.js
+++ seeds/roles.js
@@ -1,20 +1,50 @@
 module.exports.roles = {
-    heretic: {
-        name: 'heretic',
-        isAdmin: false,
-        isType: 'common',
-        description: 'Un hereje mas, no merece ni ser llamado pecador, llegó este lugar de casualidad. tiene miedo de aceptar sus pecados, pero si lo hace puede que tenga un futuro aquí.',
+    gods: {
+        name: 'gods',
+        isAdmin: true,
+        isType: 'imposible',
+        description: 'Puto el que es god',
     },
-    redskin: {
-        name: 'redskin',
-        isAdmin: false,
-        isType: 'common',
-        description: 'El demonio de piel roja. Las escamas arden, la sangre brota, el fuego quema las heridas y antes de que puedas siquiera emitir un ruido de dolor tu piel se a enrojecido permanentemente.',
+    apostol: {
+        name: 'apostol',
+        isAdmin: true,
+        isType: 'raro',
+        description: 'Fiel sirviente y mano derecha de los dioses, antiguo demionio que ha logrado mantener el orden en las eternas salas del infierno. Es el encargado de ver que se acaten la ordenes y directivas de los dioses',
     },
     winged: {
         name: 'winged',
         isAdmin: false,
-        isType: 'common',
-        description: 'Un demonio en su fase de transformación final. Los gritos de dolor llenan las habitaciones vacías, la sangre derramada alienta a las criaturas de la noche, y finalmente el demonio que logró llegar hasta el mas alto de los escalones de la escalera que atraviesa las salas del infierno, levanta vuelo hasta las altas salas del infierno.',
+        isType: 'poco común',
+        description: 'Un demonio en su fase de transformación final. Los gritos de dolor llenan las habitaciones vacías, la sangre derramada alienta a las criaturas de la noche, y finalmente el demonio que logró llegar hasta el mas alto de los escalones de la escalera que atraviesa las salas del infierno, levanta vuelo hasta las altas salas del infierno',
+    },
+    redskin: {
+        name: 'redskin',
+        isAdmin: false,
+        isType: 'común',
+        description: 'El demonio de piel roja. Las escamas arden, la sangre brota, el fuego quema las heridas y antes de que puedas siquiera emitir un ruido de dolor tu piel se a enrojecido permanentemente',
+    },
+    scaly: {
+        name: 'scaly',
+        isAdmin: false,
+        isType: 'común',
+        description: 'El demonio escamoso. La piel agangrenada cae para dar paso a las escamas que brotan, destrozando carne y tejidos'
+    },
+    sinner: {
+        name: 'sinner',
+        isAdmin: false,
+        isType: 'común',
+        description: 'Un simple pecador que es torturado por sus ofensas, pero quién sabe, tal vez luego de un tiempo se vuelva mas interesante'
+    },
+    heretic: {
+        name: 'heretic',
+        isAdmin: false,
+        isType: 'básico',
+        description: 'Un hereje mas, no merece ni ser llamado pecador, llegó este lugar de casualidad. tiene miedo de aceptar sus pecados, pero si lo hace puede que tenga un futuro aquí',
+    },
+    rebel: {
+        name: 'rebel',
+        isAdmin: false,
+        isType: 'vergonzoso',
+        description: 'Un demonio descarriado...'
     }
 }
